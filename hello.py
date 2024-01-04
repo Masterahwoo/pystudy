0a15b5ab917a62e9cace47c582f0789b3a12226b
meow <39899314+Masterahwoo@users.noreply.github.com>
2024-01-04 18:05:02 +0800

    1/4/2024

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-first_name = "Jingyan"
-last_name = "Xu"
-full_name=f"{first_name} {last_name}"
-print(full_name)
+# # 2.3
+# name = "jingyan"
+# msg= f"Hello {name.title()}, welcome"
+# print(msg)
+
+# # 2.4
+# name = "xu JIngyan"
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+
+x,y,z = 0,1,3
+print(x,"\n",y,"\n",z)
+
+
+
